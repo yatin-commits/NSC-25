be4913a89b7626f5a1c69d783ba619643ccbd10a
Yatin Sharma <sharmayatin0882@gmail.com>
2025-03-07 15:43:09 +0530

    fixing nav bar build issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../Home";
 import AdminPanel from "./components/AdminPanel";
+// import Navbar from "./components/Navbar";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import ProtectedRoute from "./components/ProtectedRoute";
@@ -9,9 +10,9 @@ import ProtectedRoute from "./components/ProtectedRoute";
 const App = () => {
   return (
     <>
-      <Navbar />
+      <Navbar/>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home/>} />
         <Route
           path="/admin"
           element={
