4d735623218c68fcff2b6def44a51ae4542fa829
Yatin Sharma <sharmayatin0882@gmail.com>
2025-03-18 20:11:40 +0530

    fixed coordinators on large screen

--- frontend/src/components/Coordinators.jsx
+++ frontend/src/components/Coordinators.jsx
@@ -2,14 +2,14 @@ import Cards from "./Cards";
 
 const Coordinators = () => {
   return (
-    <div className="flex flex-col items-center gap-y-8 py-12 px-4 bg-gradient-to-b from-purple-50 to-indigo-100 dark:from-gray-800 dark:to-gray-900">
+    <div className="flex b flex-col items-center gap-y-8 py-12 px-4 bg-gradient-to-b from-purple-50 to-indigo-100 dark:from-gray-800 dark:to-gray-900">
       {/* Title */}
       <h1 className="text-3xl sm:text-4xl lg:text-5xl font-extrabold text-gray-900 dark:text-white text-center">
         Event Coordinators
       </h1>
 
       {/* Cards Section */}
-      <div className="w-full max-w-6xl">
+      <div className="w-screen ">
         <Cards />
       </div>
     </div>
