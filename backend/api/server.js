8a649479fc61adcb91f1035218b76eb6712175a1
Yatin Sharma <sharmayatin0882@gmail.com>
2026-03-25 22:43:48 +0530

    abcd

--- backend/api/server.js
+++ backend/api/server.js
@@ -36,5 +36,7 @@ const memberRoutes = require("../routes/memberRoutes");
 app.use("/api", memberRoutes);
 app.use('/api', registrationRoutes);
 
-const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 5001;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
